9e37a8d4b21b201c737b572bed624c235532cc33
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-19 23:41:02 -0300

    v0.0.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [0.0.13] 19/03/2021
+
+### Chore
+
+- Debug NPM release
+
 ## [0.0.12] 19/03/2021
 
 ### Chore
--- npm/binary.js
+++ npm/binary.js
@@ -7,7 +7,7 @@ const error = (msg) => {
   process.exit(1);
 };
 
-const { version, name, repository } = require("./package.json");
+const { version, name, repository } = require("../package.json");
 
 const supportedPlatforms = [
   {
