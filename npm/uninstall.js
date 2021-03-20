042f924e3b20f34e81a32adb82c7c65b056e16cf
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-19 23:23:52 -0300

    v0.0.9

--- npm/uninstall.js
+++ npm/uninstall.js
@@ -1,12 +1,2 @@
-function get() {
-  try {
-    const getBinary = require("./getBinary");
-    return getBinary();
-  } catch (err) {}
-}
-
-const binary = get();
-
-if (binary) {
-  binary.uninstall();
-}
+const getBinary = require("./getBinary");
+getBinary().uninstall();
