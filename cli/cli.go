acbc5ac2bc99b91e820ca783a4ff130d17ce3658
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-06-03 16:08:44 -0300

    fix: bump cli version before releasing new tag

--- cli/cli.go
+++ cli/cli.go
@@ -18,7 +18,7 @@ func Kontrolio() {
 	app := &cli.App{
 		Name:    "kontrolio",
 		Usage:   "Your cli time clock, clock card machine, punch clock or time recorder",
-		Version: "0.0.35",
+		Version: "0.0.36",
 
 		Commands: []*cli.Command{
 			{
