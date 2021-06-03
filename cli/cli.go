5b247b12623890b9cce8bbd2a18266b911c5863a
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-06-03 19:04:53 -0300

    v0.0.37

--- cli/cli.go
+++ cli/cli.go
@@ -18,7 +18,7 @@ func Kontrolio() {
 	app := &cli.App{
 		Name:    "kontrolio",
 		Usage:   "Your cli time clock, clock card machine, punch clock or time recorder",
-		Version: "0.0.36",
+		Version: "0.0.37",
 
 		Commands: []*cli.Command{
 			{
