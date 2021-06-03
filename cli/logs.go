59f510cce0b51c90904b7f3cb54f8c36d1384106
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-06-03 18:46:10 -0300

    feat: logBuilder

--- cli/logs.go
+++ cli/logs.go
@@ -14,60 +14,18 @@ import (
 func logs(tail *string) {
 	records := db.GetRecords()
 
-	var currentDay *time.Time
 	var log []string
-	workWindowNanoseconds := int64(0)
-	workNanoseconds := int64(0)
 
-	for index, parsedRecord := range records {
-		isLastRecord := index+1 == len(records)
-
-		if currentDay == nil {
-			endOfDay := utils.EndOfDay(parsedRecord.Time)
-			currentDay = &endOfDay
-			log = append(log, messages.FormatLogMessageHeader(currentDay))
-		}
-
-		if parsedRecord.Time.After(*currentDay) {
+	logBuilder(records, func(workNanoseconds *int64, workWindowNanoseconds *int64, currentDay *time.Time, record *utils.Record) {
+		if workNanoseconds != nil && workWindowNanoseconds != nil {
 			log = append(log,
-				messages.FormatLogMessageFooter(time.Duration(workNanoseconds).String(), time.Duration(workWindowNanoseconds).String()))
-
-			// Resets time accumulators
-			workWindowNanoseconds = 0
-			workNanoseconds = 0
-
-			endOfDay := utils.EndOfDay(parsedRecord.Time)
-			currentDay = &endOfDay
+				messages.FormatLogMessageFooter(time.Duration(*workNanoseconds).String(), time.Duration(*workWindowNanoseconds).String()))
+		} else if record == nil && currentDay != nil {
 			log = append(log, messages.FormatLogMessageHeader(currentDay))
+		} else {
+			log = append(log, messages.FormatLogMessage(*record))
 		}
-
-		if !isLastRecord && records[index+1].Time.Before(*currentDay) {
-			workWindowNanoseconds = workWindowNanoseconds + utils.SubtractTime(parsedRecord.Time, records[index+1].Time)
-		}
-
-		log = append(log, messages.FormatLogMessage(parsedRecord))
-
-		// We compute worked hours from records of type
-		// "out"
-		if parsedRecord.Type == db.RecordTypeRegistry.Out {
-			workNanoseconds = workNanoseconds + utils.SubtractTime(records[index-1].Time, parsedRecord.Time)
-		}
-
-		// This condition means that we reached the last log,
-		// thus we must append the accumulated time for the last
-		// day here
-		if isLastRecord {
-			// Covers the cases where the client has punched in but haven't
-			// punched out yet, so we compute how much time has passed
-			// between when it punched in and now.
-			if parsedRecord.Type == db.RecordTypeRegistry.In {
-				workNanoseconds = workNanoseconds + utils.SubtractTime(parsedRecord.Time, time.Now())
-			}
-
-			log = append(log,
-				messages.FormatLogMessageFooter(time.Duration(workNanoseconds).String(), time.Duration(workWindowNanoseconds).String()))
-		}
-	}
+	})
 
 	if tail != nil {
 		if tail, err := strconv.Atoi(*tail); err == nil && tail > 0 {
@@ -109,4 +67,56 @@ func logs(tail *string) {
 	}
 
 	fmt.Println()
+}
+
+func logBuilder(records []utils.Record, logger func(workNanoseconds *int64, workWindowNanoseconds *int64, currentDay *time.Time, record *utils.Record)) {
+	workWindowNanoseconds := int64(0)
+	workNanoseconds := int64(0)
+	var currentDay *time.Time
+
+	for index, record := range records {
+		isLastRecord := index+1 == len(records)
+
+		if currentDay == nil {
+			endOfDay := utils.EndOfDay(record.Time)
+			currentDay = &endOfDay
+			logger(nil, nil, currentDay, nil)
+		}
+
+		if record.Time.After(*currentDay) {
+			logger(&workNanoseconds, &workWindowNanoseconds, nil, nil)
+
+			// Resets time accumulators
+			workWindowNanoseconds = 0
+			workNanoseconds = 0
+
+			logger(nil, nil, currentDay, nil)
+		}
+
+		if !isLastRecord && records[index+1].Time.Before(*currentDay) {
+			workWindowNanoseconds = workWindowNanoseconds + utils.SubtractTime(record.Time, records[index+1].Time)
+		}
+
+		logger(nil, nil, nil, &record)
+
+		// We compute worked hours from records of type
+		// "out"
+		if record.Type == db.RecordTypeRegistry.Out {
+			workNanoseconds = workNanoseconds + utils.SubtractTime(records[index-1].Time, record.Time)
+		}
+
+		// This condition means that we reached the last log,
+		// thus we must append the accumulated time for the last
+		// day here
+		if isLastRecord {
+			// Covers the cases where the client has punched in but haven't
+			// punched out yet, so we compute how much time has passed
+			// between when it punched in and now.
+			if record.Type == db.RecordTypeRegistry.In {
+				workNanoseconds = workNanoseconds + utils.SubtractTime(record.Time, time.Now())
+			}
+
+			logger(&workNanoseconds, &workWindowNanoseconds, nil, nil)
+		}
+	}
 }
